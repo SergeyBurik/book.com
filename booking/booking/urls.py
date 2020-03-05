8fa01d20827b34ff56a531f5a389190d91d593c0
Sergio Burik <sergioburik@gmail.com>
2020-03-05 19:34:50 +0300

    settings

--- booking/booking/urls.py
+++ booking/booking/urls.py
@@ -19,3 +19,8 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
