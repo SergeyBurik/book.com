68c7891ecb85a35885f1c20a7899057a5301ce4d
Sergio Burik <sergioburik@gmail.com>
2020-03-06 21:03:08 +0300

    Revert "Merge branch 'dev' of https://github.com/SergeyForester/book.com into dev"
    This reverts commit c229da573df415a3229318fcc7c4ab0d2c7f27f0, reversing
    changes made to b333968a6fc1b80fafae690c2c664312d75cfde3.

--- booking/authapp/urls.py
+++ booking/authapp/urls.py
@@ -6,6 +6,5 @@ app_name = 'authapp'
 
 urlpatterns = [
      path('join/', authapp.join, name='join'),  # sign up page url
-     path('login/', authapp.login, name='login'),  # sign in page url
 
 ]
--- booking/authapp/views.py
+++ booking/authapp/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 
 def join(request):
     return render(request, 'authapp/sign_up.html', {})
-
-def login(request):
-    return render(request, 'authapp/sign_in.html', {})
