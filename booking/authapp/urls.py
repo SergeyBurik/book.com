07b31e322273f08d973f7ecba487d33a430ff1de
Sergio Burik <sergioburik@gmail.com>
2020-03-06 20:29:31 +0300

    sign in page

--- booking/authapp/urls.py
+++ booking/authapp/urls.py
@@ -6,5 +6,6 @@ app_name = 'authapp'
 
 urlpatterns = [
      path('join/', authapp.join, name='join'),  # sign up page url
+     path('login/', authapp.login, name='login'),  # sign in page url
 
 ]
