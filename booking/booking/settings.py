02915480574b537e076b27a1482a0086b51d27d8
Sergio Burik <sergioburik@gmail.com>
2020-03-07 22:06:40 +0300

    letter

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -131,3 +131,10 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'testmailbook@mail.ru'
+EMAIL_HOST_PASSWORD = 'StrongPassword'
