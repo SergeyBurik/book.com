5559ade955d3f723dbb276f3acc96e2f3bf2cf85
Sergio Burik <sergioburik@gmail.com>
2020-03-07 20:38:31 +0300

    c

--- booking/authapp/views.py
+++ booking/authapp/views.py
@@ -1,8 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from authapp.forms import UserAdminCreationForm
 
 
 def join(request):
-    return render(request, 'authapp/sign_up.html', {})
+    if request.method == 'POST':
+        register_form = UserAdminCreationForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('main'))
+    else:
+        register_form = UserAdminCreationForm()
+
+    content = {'register_form': register_form}
+
+    return render(request, 'authapp/sign_up.html', content)
+
 
 def login(request):
-	return render(request, 'authapp/sign_in.html', {})
+    return render(request, 'authapp/sign_in.html', {})
