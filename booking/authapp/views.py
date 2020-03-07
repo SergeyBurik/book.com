ed5cbd7f29cc3ef46e8a45ca9429f6c6f190be87
iizakharov <45385589+iizakharov@users.noreply.github.com>
2020-03-07 20:33:56 +0300

    fix errors PEP8

--- booking/authapp/views.py
+++ booking/authapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def join(request):
-    return render(request, 'authapp/sign_up.html', {})
+	return render(request, 'authapp/sign_up.html', {})
+
 
 def login(request):
 	return render(request, 'authapp/sign_in.html', {})
