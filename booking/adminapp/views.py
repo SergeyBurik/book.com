721573edbb5b8dfa4c03921ded4ab8e1fc6eb5b9
Sergio Burik <sergioburik@gmail.com>
2020-03-14 14:55:56 +0300

    c

--- booking/adminapp/views.py
+++ booking/adminapp/views.py
@@ -1,9 +1,24 @@
+import datetime
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from mainapp.models import Bookings, Hotel
 
-
-# Create your views here.
 
 @login_required(login_url='/auth/login/')
 def main(request):
-    return render(request, 'adminapp/main.html', {})
+    hotels = Hotel.objects.filter(user=request.user)
+    bookings = []
+
+    days = [datetime.date.today() + datetime.timedelta(days=dayR) for dayR in range(14)]
+
+    for hotel in hotels:
+        bookings += Bookings.objects.filter(hotel=hotel)
+
+    context = {'bookings': bookings, 'hotels': hotels, 'days': days}
+
+    return render(request, 'adminapp/main.html', context)
+
+
+@login_required(login_url='/auth/login/')
+def create_hotel(request):
+    return render(request, 'adminapp/create_hotel.html')
