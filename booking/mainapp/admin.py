909265f5f5e54c5ea051e39f6d7b08ebd6485935
iizakharov <45385589+iizakharov@users.noreply.github.com>
2020-03-20 16:21:00 +0300

    Add files via upload

--- booking/mainapp/admin.py
+++ booking/mainapp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from mainapp.models import Hotel, Room, RoomGallery, RoomAgent
 
-# Register your models here.
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    search_fields = 'name',
+
+
+@admin.register(RoomAgent)
+class RoomManagerAdmin(admin.ModelAdmin):
+    search_fields = 'name',
+
+
+class RoomGalleryInline(admin.TabularInline):
+    model = RoomGallery
+    extra = 3
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    inlines = RoomGalleryInline,
+    search_fields = 'name', 'hotel__name',
+    list_display = 'name',
+
+
+@admin.register(RoomGallery)
+class RoomGalleryAdmin(admin.ModelAdmin):
+    search_fields = 'room',
+    list_display = 'room', 'image',
--- booking/mainapp/views.py
+++ booking/mainapp/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def main_page(request):
-    return render(request, 'mainapp/index.html', {})
+    user = request.user
+
+    return render(request, 'mainapp/index.html', {'user': user})
