54f0b775231425dadc457e459c313ed6ab6c938e
Sergio Burik <sergioburik@gmail.com>
2020-03-23 18:15:39 +0300

    bookings main

--- booking/mainapp/views.py
+++ booking/mainapp/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+import datetime
+
+from django.shortcuts import render, get_object_or_404
+from mainapp.models import Hotel, Room
 
 
 def main_page(request):
     user = request.user
 
     return render(request, 'mainapp/index.html', {'user': user})
+
+
+def bookings_main(request, hotel_id):
+    hotel = get_object_or_404(Hotel, pk=hotel_id)
+    rooms = Room.objects.filter(hotel=hotel, is_active=True)
+    days = [datetime.date.today() + datetime.timedelta(days=dayR) for dayR in range(14)]
+
+    return render(request, 'mainapp/booking_main.html', {'hotel': hotel,
+                                                         'rooms': rooms,
+                                                         'days': days})
