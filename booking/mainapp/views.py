9dc6bf07e4ff28323c57e0ad304e91e418b88c04
Sergio Burik <sergioburik@gmail.com>
2020-03-23 20:20:05 +0300

    book  room

--- booking/mainapp/views.py
+++ booking/mainapp/views.py
@@ -18,3 +18,15 @@ def bookings_main(request, hotel_id):
     return render(request, 'mainapp/booking_main.html', {'hotel': hotel,
                                                          'rooms': rooms,
                                                          'days': days})
+
+
+def book_room(request, hotel_id, room_id):
+    user = request.user
+    hotel = get_object_or_404(Hotel, pk=hotel_id)
+    room = get_object_or_404(Room, hotel=hotel, pk=room_id, is_active=True)
+    days = [datetime.date.today() + datetime.timedelta(days=dayR) for dayR in range(14)]
+
+    return render(request, 'mainapp/book_room.html', {'user': user,
+                                                      'hotel': hotel,
+                                                      'room': room,
+                                                      'days': days})
