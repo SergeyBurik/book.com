68bcdc9aca7afcf878ab7008934a6a93df75036c
Sergio Burik <sergioburik@gmail.com>
2020-04-10 17:03:25 +0300

    api

--- booking/apiapp/urls.py
+++ booking/apiapp/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('getRooms/', apiapp.get_rooms, name='get_rooms'),
     path('getHotel/', apiapp.get_hotel, name='get_hotel'),
     path('getRoom/', apiapp.get_room, name='get_room'),
+    path('getBookings/', apiapp.get_bookings, name='get_bookings'),
+    path('getHotelBookings/', apiapp.get_hotel_bookings, name='get_hotel_bookings'),
     path('createBooking/', apiapp.create_booking, name='create_booking'),
 ]
