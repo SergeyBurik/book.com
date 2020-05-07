2450faeacbbcf0d052019b96eebeeca3d27758f2
iizakharov <45385589+iizakharov@users.noreply.github.com>
2020-05-07 11:31:20 +0300

    new models

--- booking/mainapp/models.py
+++ booking/mainapp/models.py
@@ -48,26 +48,37 @@ class Hotel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     name = models.CharField(verbose_name='Название отеля', max_length=64,
                             unique=True)
-    phone_number = models.CharField(verbose_name='Номер телефона', default='', max_length=20)
-    location = models.CharField(verbose_name='Адрес отеля', default='', max_length=200)
+    phone_number = models.CharField(verbose_name='Номер телефона', default='',
+                                    max_length=20)
+    location = models.CharField(verbose_name='Адрес отеля', default='',
+                                max_length=200)
     description = models.TextField(verbose_name='Описание отеля', blank=True)
     stars = models.CharField(max_length=2, choices=STARS_CHOICES, default=ONE)
-    banner = models.ImageField(default='', upload_to='hotels/banners/')  # hotel's image
+    banner = models.ImageField(default='', upload_to='hotels/banners/')
     is_active = models.BooleanField(verbose_name='Активен', default=True)
 
     def __str__(self):
         return self.name
 
+    def get_count_facility(self):
+        items = self.hotelfacility.select_related()
+        return len(items)
+
 
 class Facility(models.Model):
     icon = models.ImageField(default='', upload_to='hotels/icons/')
-    name = models.CharField(verbose_name='Название удобства', max_length=64,
+    name = models.CharField(verbose_name='Facility', max_length=64,
                             unique=True)
 
 
 class HotelFacility(models.Model):
-    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, default='')
-    facility = models.ForeignKey(Facility, on_delete=models.CASCADE, default='')
+    hotel = models.ForeignKey(Hotel, related_name="hotelfacility",
+                              on_delete=models.CASCADE)
+    facility = models.ForeignKey(Facility, verbose_name='Facility name',
+                                 on_delete=models.CASCADE)
+
+    def get_icon(self):
+        return self.facility.icon
 
 
 class HotelComfort(models.Model):
